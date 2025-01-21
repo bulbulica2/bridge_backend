cceb0977f8da8b816c59f2a16ce1d9c25a265ecb
bulbulica2 <57604487+bulbulica2@users.noreply.github.com>
2025-01-21 15:42:39 +0200

    2-laravel-testing - added new routes for table and card

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\TableController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+  return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+// not important
+Route::get('/cards', [CardController::class, 'getCards']);
+// not important
+Route::get('/card/{id}', [CardController::class, 'getCard']);
+// not important
+Route::get('/table/{id}', [TableController::class, 'index']);
+
+Route::post('/create_table', [TableController::class, 'createNewTable']); // ->middleware('auth:sanctum')
+
+require __DIR__ . '/auth.php';
